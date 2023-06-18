3bddba7c8b97bfd2d8306c2efbbb0ad9b2773f0f
yn9634 <sanghun9634@gmail.com>
2023-06-18 12:40:20 +0900

    fetch: print feed_find check error

--- Google_trand_slack_bot.py
+++ Google_trand_slack_bot.py
@@ -23,6 +23,8 @@ def get_now_google_trand():
     global trand_list
     global server_now
     feed_list = []
+    feed_find = []
+    
     
     day = str(server_now.day)+"일"
 
@@ -42,18 +44,24 @@ def get_now_google_trand():
                 feed_find = feed.find_elements(By.CLASS_NAME, "md-list-block")
                 break;
 
-        for feed in feed_find: 
-            title = feed.find_element(By.CLASS_NAME, "title").text
-            
-            if title in trand_list:
-                pass
-            else:
-                content = feed.find_element(By.CLASS_NAME, "summary-text").text
-                url = feed.find_element(By.TAG_NAME, "feed-item").get_attribute("share-url")
-                info = feed.find_element(By.CLASS_NAME, "source-and-time").get_attribute("title")
-                feed_list.append('{} \n{} \n{} \n{}'.format(title, content, url, info))
-                trand_list.append(title)
+        print(feed_find)
+        if len(feed_find) == 0:
+            pass
         
+        else:
+            for feed in feed_find: 
+                title = feed.find_element(By.CLASS_NAME, "title").text
+                
+                if title in trand_list:
+                    pass
+                else:
+                    content = feed.find_element(By.CLASS_NAME, "summary-text").text
+                    url = feed.find_element(By.TAG_NAME, "feed-item").get_attribute("share-url")
+                    info = feed.find_element(By.CLASS_NAME, "source-and-time").get_attribute("title")
+                    feed_list.append('{} \n{} \n{} \n{}'.format(title, content, url, info))
+                    trand_list.append(title)
+
+
 
         return feed_list
 
@@ -64,24 +72,27 @@ def send_slack_message(bot_url, day):
 
     feed_list = get_now_google_trand()
 
+    if len(feed_list) == 0:
+        pass
         
-    for feed in feed_list:
-        payload = {
-            "text": feed
-        }
-        # payload =  { "name" : "Lee Morgan",
-        #   "interviewer":"interviewed by: <a href='http://onehungrymind.com/angularjs-dynamic-templates/'>Sonny Stitt</a>",
-        #   "day" : "Saturday",
-        #   "date": "April 18th", 
-        # }
-        #get, post => CRUD
-        response = requests.post(
-            bot_url,
-            data=json.dumps(payload),
-            headers={"Content-Type":"application/json"}
-        )
-        print("server_time:{} \t resopone:{}".format(server_now,response))
-         
+    else:
+        for feed in feed_list:
+            payload = {
+                "text": feed
+            }
+            # payload =  { "name" : "Lee Morgan",
+            #   "interviewer":"interviewed by: <a href='http://onehungrymind.com/angularjs-dynamic-templates/'>Sonny Stitt</a>",
+            #   "day" : "Saturday",
+            #   "date": "April 18th", 
+            # }
+            #get, post => CRUD
+            response = requests.post(
+                bot_url,
+                data=json.dumps(payload),
+                headers={"Content-Type":"application/json"}
+            )
+            print("server_time:{} \t resopone:{}".format(server_now,response))
+            
     #print(trand_list)
     #print(feed_list)
     #https://api.slack.com/messaging/composing
