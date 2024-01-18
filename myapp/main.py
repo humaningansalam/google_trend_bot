535cc789f90ab4a9e8f281d46c918f44035054ec
humaningansalam <sanghun9634@gmail.com>
2024-01-18 22:56:05 +0900

    feat: logging update
    console log add  and  update log display

--- myapp/main.py
+++ myapp/main.py
@@ -22,8 +22,24 @@ class GoogleTrendsBot:
         self.trand_list = []
         self.reset_done = False
 
-        # 로거 설정
-        logging.basicConfig(filename='GoogleTrendsBot.log', level=logging.INFO)
+        # 로그 생성
+        logger = logging.getLogger()
+
+        # 로그의 출력 기준 설정
+        logger.setLevel(logging.INFO)
+
+        # log 출력 형식
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # log를 console에 출력
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # log를 파일에 출력
+        file_handler = logging.FileHandler('GoogleTrendsBot.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
         # 크롬 인스턴스 생성
         self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
