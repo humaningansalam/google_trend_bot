38e73f6786d58ee0af59728e8f784da6b80869a8
sanghun9634 <sanghun9634@gmail.com>
2024-11-05 13:32:52 +0900

    chore: headless browser on

--- myapp/src/bot/GoogleTrendsScraper.py
+++ myapp/src/bot/GoogleTrendsScraper.py
@@ -6,7 +6,7 @@ class Scraper:
         pass
         
     def setup_browser(self, playwright):
-        browser = playwright.chromium.launch(headless=False)
+        browser = playwright.chromium.launch(headless=True)
         context = browser.new_context(
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         )
